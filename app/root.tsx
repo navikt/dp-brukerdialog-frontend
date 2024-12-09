118a177d79af25c464e28dc76611eb6b35232542
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2024-12-09 15:31:24 +0100

    Legge til nais config, github action. Fikse basePath

--- app/root.tsx
+++ app/root.tsx
@@ -1,13 +1,5 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
-
-import "./tailwind.css";
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { vitePlugin as remix } from "@remix-run/dev";
+import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+
+installGlobals();
 
 declare module "@remix-run/node" {
   interface Future {
@@ -8,9 +11,13 @@ declare module "@remix-run/node" {
   }
 }
 
+const basePath = "/arbeid/dagpenger/brukerdialog-frontend/";
+
 export default defineConfig({
+  base: basePath,
   plugins: [
     remix({
+      basename: basePath,
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
