90591906fdfa3bceff59bb637103d56116f336d9
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2024-12-09 16:31:58 +0100

    Legge til Dekoratør, favicon

---
+++ app/models/decorator.server.ts
@@ -0,0 +1,30 @@
+import {
+  DecoratorElements,
+  DecoratorEnvProps,
+  fetchDecoratorHtml,
+  type DecoratorFetchProps,
+} from "@navikt/nav-dekoratoren-moduler/ssr";
+import { getEnv } from "~/utils/env.utils";
+
+export async function getDecoratorHTML(): Promise<DecoratorElements> {
+  const config: DecoratorFetchProps = {
+    env: (getEnv("DEKORATOR_ENV") || "localhost") as DecoratorEnvProps["env"],
+    localUrl: "https://dekoratoren.ekstern.dev.nav.no",
+    params: {
+      language: "nb",
+      context: "privatperson",
+      chatbot: false,
+      redirectToApp: true,
+      level: "Level4",
+      breadcrumbs: [
+        { title: "Min side", url: "https://www.nav.no/minside" },
+        {
+          title: "Mine dagpenger",
+          url: "https://www.nav.no/arbeid/dagpenger/mine-dagpenger",
+        },
+      ],
+    },
+  };
+
+  return await fetchDecoratorHtml(config);
+}
--- app/root.tsx
+++ app/root.tsx
@@ -1,31 +1,81 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
+import navStyles from "@navikt/ds-css/dist/index.css?url";
+import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useRouteLoaderData,
+} from "@remix-run/react";
+import parse from "html-react-parser";
+import { typedjson, useTypedRouteLoaderData } from "remix-typedjson";
+import { useInjectDecoratorScript } from "./hooks/useInjectDecoratorScript";
+import { getDecoratorHTML } from "./models/decorator.server";
+import { getEnv } from "./utils/env.utils";
 
 export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  { rel: "stylesheet", href: navStyles },
   {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
+    rel: "icon",
+    type: "image/png",
+    sizes: "32x32",
+    href: "favicon-32x32.png",
   },
   {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    rel: "icon",
+    type: "image/png",
+    sizes: "16x16",
+    href: "favicon-16x16.png",
+  },
+  {
+    rel: "icon",
+    type: "image/x-icon",
+    href: "favicon.ico",
   },
 ];
 
+export async function loader({ request }: LoaderFunctionArgs) {
+  const decoratorFragments = await getDecoratorHTML();
+
+  if (!decoratorFragments) {
+    throw typedjson({ error: "Kunne ikke hente dekoratør" }, { status: 500 });
+  }
+
+  return typedjson({
+    decoratorFragments,
+    env: {
+      BASE_PATH: getEnv("BASE_PATH"),
+      APP_ENV: getEnv("APP_ENV"),
+      SANITY_DATASET: getEnv("SANITY_DATASET"),
+    },
+  });
+}
+
 export function Layout({ children }: { children: React.ReactNode }) {
+  const { decoratorFragments, env } = useTypedRouteLoaderData("root");
+
+  useInjectDecoratorScript(decoratorFragments.DECORATOR_SCRIPTS);
+
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {parse(decoratorFragments.DECORATOR_HEAD_ASSETS, { trim: true })}
         <Meta />
         <Links />
       </head>
       <body>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.env = ${JSON.stringify(env)}`,
+          }}
+        />
+        {parse(decoratorFragments.DECORATOR_HEADER, { trim: true })}
         {children}
         <ScrollRestoration />
+        {parse(decoratorFragments.DECORATOR_FOOTER, { trim: true })}
         <Scripts />
       </body>
     </html>
