06b8794cdd69dcc621f928088327e1f79b54fe78
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2024-12-12 17:20:49 +0100

    Legge til dp-soknad-orkestrator url i nais config

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,11 @@
 import { redirect, type LoaderFunctionArgs } from "@remix-run/node";
 
 export async function loader({ params }: LoaderFunctionArgs) {
-  // return redirect(`/13eaa299-ad5c-432c-a207-2c796274d309`);
+  return redirect(`/13eaa299-ad5c-432c-a207-2c796274d309`);
 
-  if (!params.soknadId) {
-    return redirect("/error");
-  }
+  // if (!params.soknadId) {
+  //   return redirect("/error");
+  // }
 
-  return {};
+  // return {};
 }
