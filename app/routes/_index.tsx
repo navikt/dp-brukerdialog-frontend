16fb9f1d45e577aeb17d6cec37d57f66e51f613f
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2024-12-12 16:58:57 +0100

    Installere pini-pretty og legge til automatisk redirect til dummy søknadId

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,11 @@
 import { redirect, type LoaderFunctionArgs } from "@remix-run/node";
 
 export async function loader({ params }: LoaderFunctionArgs) {
-  if (!params.soknadId) {
-    return redirect("/error");
-  }
+  return redirect(`/13eaa299-ad5c-432c-a207-2c796274d309`);
 
-  return {};
+  // if (!params.soknadId) {
+  //   return redirect("/error");
+  // }
+
+  // return {};
 }
