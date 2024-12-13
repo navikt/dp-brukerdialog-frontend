5dcda36e31ca6aa4a95c89d7acf075ff5253c930
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2024-12-13 14:20:35 +0100

    Legge tilbake deploy prod og legg til redirect til /prepod dersom vi får inn bruker i prod

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,14 @@
 import { redirect, type LoaderFunctionArgs } from "@remix-run/node";
+import { getEnv } from "~/utils/env.utils";
 
 export async function loader({ params }: LoaderFunctionArgs) {
-  return redirect(`/13eaa299-ad5c-432c-a207-2c796274d309`);
+  const prod = getEnv("APP_ENV") === "production";
 
+  if (prod) {
+    return redirect("/prepod");
+  }
+
+  return redirect(`/13eaa299-ad5c-432c-a207-2c796274d309`);
   // if (!params.soknadId) {
   //   return redirect("/error");
   // }
