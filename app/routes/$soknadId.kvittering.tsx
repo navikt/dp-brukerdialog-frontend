71574b1794b252ac2d704257d40a576e1520de4f
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2025-01-07 13:28:15 +0100

    Vise last opp dokumentasjon basert på svartet bruker har gitt

--- app/routes/$soknadId.kvittering.tsx
+++ app/routes/$soknadId.kvittering.tsx
@@ -4,6 +4,7 @@ import { useLoaderData } from "@remix-run/react";
 import invariant from "tiny-invariant";
 import { ExternalLink } from "~/components/ExternalLink";
 import { getMinsteinntektForeleggingresultat } from "~/models/getMinsteinntektForeleggingresultat.server";
+import {typedjson} from "remix-typedjson";
 
 export const meta: MetaFunction = () => {
   return [
@@ -20,43 +21,79 @@ export async function loader({ params, request }: LoaderFunctionArgs) {
     params.soknadId
   );
 
-  return { minsteinntektForeleggingresultat };
+  return typedjson({ minsteinntektForeleggingresultat });
 }
 
 export default function Kvittering() {
   const { minsteinntektForeleggingresultat } = useLoaderData<typeof loader>();
 
+  const inntektStemmer  = minsteinntektForeleggingresultat.data.bekreftet;
+
+
   if (minsteinntektForeleggingresultat.status === "error") {
     return <p>Error</p>;
   }
 
-  const inntektIkkeStemmer = !minsteinntektForeleggingresultat.data.bekreftet ? "ikke" : "";
+  function InntektStemmer() {
+      return (
+          <BodyLong spacing className="mt-8">
+              Vi har mottatt oppgaven din om inntektsopplysninger.
+              <br />
+              <br />
+              Vi har fått svaret ditt om at inntekten din stemmer.
+              <br />
+              <br />
+              Du vil få svar på søknaden og beskjed når oppgaven er behandlet hos Nav.
+          </BodyLong>
+      )
+  }
+
+  function InntektIkkeStemmer() {
+      return (
+          <BodyLong spacing className="mt-8">
+              Vi har mottatt oppgaven din om inntektsopplysninger.
+              <br/>
+              <br/>
+              Vi har fått svaret ditt om at inntekten din ikke stemmer. Ta kontakt med arbeidsgiveren din for å rette
+              opplysningene om inntekten.
+              <br/>
+              <br/>
+
+              <Heading size="medium" level={"1"} id="header-icon" spacing className="mt-8">
+                  Last opp dokumentasjon
+              </Heading>
+              Du kan også laste opp dokumentasjon som bekrefter at inntekten din ikke stemmer. Det kan for eksempel være
+              lønnslipper eller årsoppgaven fra arbeidsgiver. Det viktigste er at arbeidsgiver retter opplysningen om
+              inntekten din.
+
+              <br/>
+              <br/>
+              Hvis du ikke har dokumentasjon tilgjengelig, kan du sende det senere via mine dagpenger.
+          </BodyLong>
+      )
+  }
 
   return (
     <div className="brukerdialog">
       <Heading size="large" level={"1"}>
-        Kvittering
+        Svar om inntekt mottatt
       </Heading>
 
-      <Heading size="medium" level={"2"} className="mt-4">
-        Svar på oppgave mottatt
-      </Heading>
+      <BodyShort spacing>Svar på oppgave innsendt 20.05.2024 - 12:35</BodyShort>
 
-      <BodyShort spacing> Søknad innsendt 30.05.2024 - 12:56 </BodyShort>
 
-      <BodyLong spacing className="mt-14">
-        Vi har mottatt oppgaven din om inntektsopplysninger.
-        <br />
-        <br />
-        Du svarte at inntekten din {inntektIkkeStemmer} stemmer.
-        <br />
-        <br />
-        Du vil få svar på søknaden og beskjed når oppgaven er behandlet hos Nav.
-      </BodyLong>
+        {inntektStemmer && InntektStemmer()}
+        {!inntektStemmer && InntektIkkeStemmer()}
 
-      <ExternalLink to="ettersending" asButtonVariant="primary" className="mt-14">
+      <ExternalLink to="ettersending" asButtonVariant={inntektStemmer ? "primary": "secondary"} className="mt-14">
         Gå til mine dagpenger
       </ExternalLink>
+
+        {!inntektStemmer  && (
+            <ExternalLink to="ettersending" asButtonVariant="primary" className="mt-14 ml-4">
+            Last opp dokumentasjon
+            </ExternalLink>
+        )}
     </div>
   );
 }
