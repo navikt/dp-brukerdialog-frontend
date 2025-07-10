6f3194f748d2249f22650a33e91f596f1f5fb712
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2025-07-10 13:15:56 +0200

    Fikse dekoratør console error

--- app/root.tsx
+++ app/root.tsx
@@ -11,11 +11,12 @@ import {
 import parse from "html-react-parser";
 import { useInjectDecoratorScript } from "./hooks/useInjectDecoratorScript";
 import { getDecoratorHTML } from "./models/decorator.server";
-import { getEnv } from "./utils/env.utils";
 import { logger } from "./utils/logger.utils";
 
 import akselStyles from "@navikt/ds-css/dist/index.css?url";
 import indexStyles from "./index.css?url";
+import { getEnv } from "./utils/env.utils";
+import { useEffect, useState } from "react";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: akselStyles },
@@ -42,29 +43,36 @@ export async function loader() {
 }
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const [header, setHeader] = useState<string | null>(null);
   const { decoratorFragments, env } = useLoaderData();
+  const { DECORATOR_HEAD_ASSETS, DECORATOR_SCRIPTS, DECORATOR_HEADER, DECORATOR_FOOTER } =
+    decoratorFragments;
 
-  useInjectDecoratorScript(decoratorFragments.DECORATOR_SCRIPTS);
+  useEffect(() => {
+    setHeader(DECORATOR_HEADER);
+  }, [DECORATOR_HEADER]);
+
+  useInjectDecoratorScript(DECORATOR_SCRIPTS);
 
   return (
     <html lang="no">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {parse(decoratorFragments.DECORATOR_HEAD_ASSETS, { trim: true })}
+        {parse(DECORATOR_HEAD_ASSETS, { trim: true })}
         <Meta />
         <Links />
       </head>
       <body>
+        <div dangerouslySetInnerHTML={header ? { __html: header } : undefined} />
+        {children}
+        <ScrollRestoration />
+        <div dangerouslySetInnerHTML={{ __html: DECORATOR_FOOTER }} />
         <script
           dangerouslySetInnerHTML={{
             __html: `window.env = ${JSON.stringify(env)}`,
           }}
         />
-        {parse(decoratorFragments.DECORATOR_HEADER, { trim: true })}
-        {children}
-        <ScrollRestoration />
-        {parse(decoratorFragments.DECORATOR_FOOTER, { trim: true })}
         <Scripts />
       </body>
     </html>
