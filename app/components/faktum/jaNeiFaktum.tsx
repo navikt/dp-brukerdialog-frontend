5868caba8555e43efba711f96991ebb25a19db3c
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2025-07-10 13:29:21 +0200

    Opprydding

--- app/components/faktum/jaNeiFaktum.tsx
+++ app/components/faktum/jaNeiFaktum.tsx
@@ -1,23 +1,27 @@
-import {Radio, RadioGroup} from "@navikt/ds-react";
+import { Radio, RadioGroup } from "@navikt/ds-react";
 
 interface JaNeiFaktumProps {
-    ledetekst: string;
-    beskrivelse?: string;
-    verdi?: boolean;
-    vedEndring?: (value: boolean) => void;
-    aktiv: boolean;
-    name?: string;
+  ledetekst: string;
+  beskrivelse?: string;
+  verdi?: boolean;
+  vedEndring?: (value: boolean) => void;
+  aktiv: boolean;
+  name?: string;
 }
 
 export default function JaNeiFaktum(props: JaNeiFaktumProps) {
-    return props.aktiv && <RadioGroup
+  return (
+    props.aktiv && (
+      <RadioGroup
         legend={props.ledetekst}
         description={props.beskrivelse}
         value={props.verdi}
         onChange={props.vedEndring}
         name={props.name}
-    >
+      >
         <Radio value={true}>Ja</Radio>
         <Radio value={false}>Nei</Radio>
-    </RadioGroup>;
+      </RadioGroup>
+    )
+  );
 }
