15e6baacb8dce57e45e753c8d663ca212131d822
Vegard Skjefstad <vegard.skjefstad@nav.no>
2025-08-05 12:24:41 +0200

    Hent barn som er registrert på søke fra PDL

---
+++ app/utils/formattering.util.ts
@@ -0,0 +1,7 @@
+import { format } from "date-fns";
+import { nb } from "date-fns/locale";
+
+export function formaterNorskDato(dato: Date) {
+  const formattertDato = format(dato, "MM. MMMM yyyy", { locale: nb });
+  return formattertDato.charAt(0).toUpperCase() + formattertDato.slice(1);
+}
