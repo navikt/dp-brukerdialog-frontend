ae70aab80d92f045b44a6cc553fc829c2e188358
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2025-08-07 12:19:40 +0200

    Refakturere personalia seksjon

--- app/routes/$soknadId.personalia.tsx
+++ app/routes/$soknadId.personalia.tsx
@@ -1,9 +1,16 @@
 import { ArrowLeftIcon, ArrowRightIcon } from "@navikt/aksel-icons";
-import { Button, HStack, Label, Page, VStack } from "@navikt/ds-react";
-import { ActionFunctionArgs, redirect, useNavigate, useParams } from "react-router";
+import { BodyLong, BodyShort, Button, HStack, Label, Page, VStack } from "@navikt/ds-react";
+import {
+  ActionFunctionArgs,
+  redirect,
+  useLoaderData,
+  useNavigate,
+  useParams,
+  data,
+  LoaderFunctionArgs,
+} from "react-router";
 import invariant from "tiny-invariant";
 import { hentPersonalia } from "~/models/hent-personalia.server";
-import { Route } from "../../.react-router/types/app/routes/+types/_index";
 
 interface IPersonalia {
   person: IPerson;
@@ -30,94 +37,98 @@ interface IAdresse {
   land: string;
 }
 
-export async function loader({ request }: Route.LoaderArgs) {
+export async function loader({ request }: LoaderFunctionArgs) {
   const response = await hentPersonalia(request);
-  return await response.json();
+  const personalia: IPersonalia = await response.json();
+
+  return data(personalia);
 }
 
 export async function action({ params }: ActionFunctionArgs) {
   invariant(params.soknadId, "SøknadId er påkrevd");
   const nesteSeksjonId = "bostedsland";
+
   return redirect(`/${params.soknadId}/${nesteSeksjonId}`);
 }
 
-// noinspection JSUnusedGlobalSymbols
-export default function Personalia({ loaderData }: Route.ComponentProps) {
+export default function Personalia() {
   const { soknadId } = useParams<{ soknadId: string }>();
   const navigate = useNavigate();
-  const personalia = loaderData as IPersonalia;
+
+  const personalia = useLoaderData<typeof loader>();
+  const { fornavn, mellomnavn, etternavn, ident, folkeregistrertAdresse, postAdresse } =
+    personalia.person;
+
+  const formattertIdent = ident.replace(/(.{6})(.{5})/, `$1 $2`);
+  const formattertKontonummer =
+    personalia.kontonummer && personalia.kontonummer.replace(/(.{4})(.{2})(.{5})/, "$1 $2 $3");
 
   return (
     <Page className="brukerdialog">
       <h2>Personalia</h2>
       <VStack gap="20">
         <VStack gap="4">
-          <p>
+          <BodyLong>
             Hvis opplysningene vi har om deg ikke stemmer, må du endre disse hos Folkeregisteret.
             Kontonummer kan du legge til eller endre på <a href="https://www.nav.no/">Min side</a>.
-          </p>
+          </BodyLong>
 
           <VStack gap="0">
-            <p>
-              <Label>Navn</Label>
-              <br />
-              {personalia.person.fornavn} {personalia.person.mellomnavn}{" "}
-              {personalia.person.etternavn}
-            </p>
-
-            <p>
-              <Label>Fødselsnummer</Label>
-              <br />
-              {personalia.person.ident.replace(/(.{6})(.{5})/, `$1 $2`)}
-            </p>
-
-            <p>
-              <Label>Folkeregistrert adresse</Label>
-              <br />
-              {personalia.person.folkeregistrertAdresse && (
-                <>
-                  {personalia.person.folkeregistrertAdresse.adresselinje1}{" "}
-                  {personalia.person.folkeregistrertAdresse.adresselinje1 && <br />}
-                  {personalia.person.folkeregistrertAdresse.adresselinje2}{" "}
-                  {personalia.person.folkeregistrertAdresse.adresselinje2 && <br />}
-                  {personalia.person.folkeregistrertAdresse.adresselinje3}{" "}
-                  {personalia.person.folkeregistrertAdresse.adresselinje3 && <br />}
-                  {personalia.person.folkeregistrertAdresse.postnummer}{" "}
-                  {personalia.person.folkeregistrertAdresse.poststed}{" "}
-                  {personalia.person.folkeregistrertAdresse.land && <br />}
-                  {personalia.person.folkeregistrertAdresse.land}
-                </>
-              )}
-              {!personalia.person.folkeregistrertAdresse && personalia.person.postAdresse && (
-                <>
-                  {personalia.person.postAdresse.adresselinje1}{" "}
-                  {personalia.person.postAdresse.adresselinje1 && <br />}
-                  {personalia.person.postAdresse.adresselinje2}{" "}
-                  {personalia.person.postAdresse.adresselinje2 && <br />}
-                  {personalia.person.postAdresse.adresselinje3}{" "}
-                  {personalia.person.postAdresse.adresselinje3 && <br />}
-                  {personalia.person.postAdresse.postnummer}{" "}
-                  {personalia.person.postAdresse.poststed}{" "}
-                  {personalia.person.postAdresse.land && <br />}
-                  {personalia.person.postAdresse.land}
-                </>
-              )}
-              {!personalia.person.folkeregistrertAdresse && !personalia.person.postAdresse && (
-                <>Det er ikke registert adresse på deg i Folkeregisteret.</>
-              )}
-            </p>
-
-            <p>
-              <Label>Kontonummer</Label>
-              <br />
-              {(personalia.kontonummer &&
-                personalia.kontonummer.replace(/(.{4})(.{2})(.{5})/, "$1 $2 $3")) || (
-                <span>
-                  Vi har ikke registrert kontonummeret ditt, og anbefaler at du legger det inn på{" "}
-                  <a href="https://www.nav.no/">Min side</a>.
-                </span>
-              )}
-            </p>
+            <div className="mb-4">
+              <Label as="p">Navn</Label>
+              <BodyShort>
+                {fornavn} {mellomnavn} {etternavn}
+              </BodyShort>
+            </div>
+            <div className="mb-4">
+              <Label as="p">Fødselsnummer</Label>
+              <BodyShort>{formattertIdent}</BodyShort>
+            </div>
+            <div className="mb-4">
+              <Label as="p">Fødselsnummer</Label>
+              <BodyShort>{formattertIdent}</BodyShort>
+            </div>
+            <div className="mb-4">
+              <Label as="p">Folkeregistrert adresse</Label>
+              <BodyShort>
+                {folkeregistrertAdresse && (
+                  <>
+                    {folkeregistrertAdresse.adresselinje1}{" "}
+                    {folkeregistrertAdresse.adresselinje1 && <br />}
+                    {folkeregistrertAdresse.adresselinje2}{" "}
+                    {folkeregistrertAdresse.adresselinje2 && <br />}
+                    {folkeregistrertAdresse.adresselinje3}{" "}
+                    {folkeregistrertAdresse.adresselinje3 && <br />}
+                    {folkeregistrertAdresse.postnummer} {folkeregistrertAdresse.poststed}{" "}
+                    {folkeregistrertAdresse.land && <br />}
+                    {folkeregistrertAdresse.land}
+                  </>
+                )}
+                {!folkeregistrertAdresse && postAdresse && (
+                  <>
+                    {postAdresse.adresselinje1} {postAdresse.adresselinje1 && <br />}
+                    {postAdresse.adresselinje2} {postAdresse.adresselinje2 && <br />}
+                    {postAdresse.adresselinje3} {postAdresse.adresselinje3 && <br />}
+                    {postAdresse.postnummer} {postAdresse.poststed} {postAdresse.land && <br />}
+                    {postAdresse.land}
+                  </>
+                )}
+                {!folkeregistrertAdresse && !postAdresse && (
+                  <>Det er ikke registert adresse på deg i Folkeregisteret.</>
+                )}
+              </BodyShort>
+            </div>
+            <div className="mb-4">
+              <Label as="p">Kontonummer</Label>
+              <BodyShort>
+                {formattertKontonummer || (
+                  <span>
+                    Vi har ikke registrert kontonummeret ditt, og anbefaler at du legger det inn på{" "}
+                    <a href="https://www.nav.no/">Min side</a>.
+                  </span>
+                )}
+              </BodyShort>
+            </div>
           </VStack>
 
           <HStack gap="4" className="mt-8">
