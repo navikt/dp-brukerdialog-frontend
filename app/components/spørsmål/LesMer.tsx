24ba85a9e542b7ea7452ad2c964f6b18ff864141
Vegard Skjefstad <vegard.skjefstad@nav.no>
2025-08-20 12:08:50 +0200

    Legg til manglende LesMer-tekst

--- app/components/spørsmål/LesMer.tsx
+++ app/components/spørsmål/LesMer.tsx
@@ -1,10 +1,12 @@
 import { ReadMore } from "@navikt/ds-react";
 import { KomponentBase } from "~/components/spørsmål/spørsmål.types";
+import parse from "html-react-parser";
 
 interface IProps {
   spørsmål: KomponentBase;
 }
 
 export function LesMer({ spørsmål }: IProps) {
-  return <ReadMore header={spørsmål.label}>{spørsmål.description}</ReadMore>;
+  // TODO: Få denne til å parse react-komponenter?
+  return <ReadMore header={spørsmål.label}>{parse(spørsmål?.description || "", { trim: true })}</ReadMore>;
 }
--- app/seksjon-regelsett/din-situasjon/din-situasjon.spørsmål.ts
+++ app/seksjon-regelsett/din-situasjon/din-situasjon.spørsmål.ts
@@ -39,4 +39,15 @@ export const dinSituasjonSpørsmål: KomponentType[] = [
     fraOgMed: startOfDay(subMonths(new Date(), 6)),
     tilOgMed: endOfDay(addMonths(new Date(), 3)),
   },
+  {
+    id: "hvilkenDatoSøkerDuDagpengerFraLesMer",
+    type: "lesMer",
+    label: "Dato, inntekt og beregning av dagpenger",
+    description:
+      "Vi beregner hvor mye du kan få i dagpenger basert på hva du har hatt i inntekt de siste 12 eller 36 månedene. Ønsker du å få med siste måneds inntekt, må du søke dagpenger tidligst fra den 6. i måneden etter. Årsaken er at vi får melding om inntekten din fra Skatteetaten den 5. i måneden. Hvis den 5. er en helg eller helligdag, får vi melding om inntekten din neste virkedag.<br/><br/>" +
+      "<strong>Eksempel:</strong><br/><br/>" +
+      "Hvis du søker om dagpenger fra 1. til 5. mars, vil januar bli siste måned med inntekt som tas med i beregningen av dagpengene dine. Søker du om dagpenger fra 6. mars, vil februar bli siste måned med inntekt som tas med i beregningen.<br/><br/>" +
+      "Hvis 5. mars er en lørdag, får vi inn inntekten din mandag 7. mars. Da må du søke dagpenger fra tirsdag 8. mars hvis du vil ha med inntekten din fra februar i beregningen.",
+    visHvis: (svar: DinSituasjonSvar) => svar.mottatt === "nei" || svar.mottatt === "vetikke",
+  },
 ];
