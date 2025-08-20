d5fab629ca4584f4fa1b2aa7dd6a1d25cbc1bc94
Vegard Skjefstad <vegard.skjefstad@nav.no>
2025-08-20 07:00:47 +0200

    Varselmelding i utdanning

--- app/routes/$soknadId.utdanning.tsx
+++ app/routes/$soknadId.utdanning.tsx
@@ -1,15 +1,22 @@
 import { ArrowLeftIcon, ArrowRightIcon } from "@navikt/aksel-icons";
 import { Alert, Button, HStack, Page, VStack } from "@navikt/ds-react";
 import { useForm } from "@rvf/react-router";
-import { data, Form, LoaderFunctionArgs, redirect, useLoaderData, useNavigate } from "react-router";
+import {
+  data,
+  Form,
+  LoaderFunctionArgs,
+  redirect,
+  useActionData,
+  useLoaderData,
+  useNavigate,
+} from "react-router";
 import invariant from "tiny-invariant";
-import { ExternalLink } from "~/components/ExternalLink";
-import { Spørsmål } from "~/components/spørsmål/Spørsmål";
 import { useNullstillSkjulteFelter } from "~/hooks/useNullstillSkjulteFelter";
 import { hentSeksjon } from "~/models/hentSeksjon.server";
 import { hentFormDefaultValues } from "~/utils/form.utils";
 import { lagreSeksjon } from "~/models/lagreSeksjon.server";
 import { utdanningSchema } from "~/seksjon-regelsett/utdanning/utdanning.schema";
+import { Spørsmål } from "~/components/spørsmål/Spørsmål";
 import { utdanningSpørsmål, UtdanningSvar } from "~/seksjon-regelsett/utdanning/utdanning.spørsmål";
 
 export async function loader({ request, params }: LoaderFunctionArgs) {
@@ -44,6 +51,7 @@ export async function action({ request, params }: LoaderFunctionArgs) {
 
 export default function Utdanning() {
   const loaderData = useLoaderData<typeof loader>();
+  const actionData = useActionData<typeof action>();
   const navigate = useNavigate();
 
   const form = useForm({
@@ -72,43 +80,20 @@ export default function Utdanning() {
                   return null;
                 }
                 return (
-                  <>
-                    <Spørsmål
-                      key={spørsmål.id}
-                      spørsmål={spørsmål}
-                      formScope={form.scope(spørsmål.id as keyof UtdanningSvar)}
-                    />
-                    {((spørsmål.id === "tarUtdanningEllerOpplæring" &&
-                      form.value("tarUtdanningEllerOpplæring") === "ja") ||
-                      (spørsmål.id === "planleggerÅStarteEllerFullføreStudierSamtidig" &&
-                        form.value("planleggerÅStarteEllerFullføreStudierSamtidig") === "ja")) && (
-                      <Alert variant="warning">
-                        For å få innvilget dagpenger mens du tar utdanning eller opplæring, må du
-                        sende inn{" "}
-                        <ExternalLink to="https://www.nav.no/fyllut/nav040605" className="inline">
-                          Søknad om å beholde dagpengene mens du tar utdanning eller opplæring - NAV
-                          04-06.05
-                        </ExternalLink>
-                        , i tillegg til å sende inn denne søknaden om dagpenger.
-                        <br />
-                        <br />
-                        Hvis du ikke sender søknaden om å beholde dagpengene mens du tar utdanning
-                        eller opplæring, kan vi avslå søknaden din om dagpenger.
-                      </Alert>
-                    )}
-
-                    {spørsmål.id === "dokumenterAvsluttetUtdanningSiste6MånederNå" &&
-                      form.value("dokumenterAvsluttetUtdanningSiste6MånederNå") === "nei" && (
-                        <Alert variant="warning">
-                          Du vil mest sannsynlig få avslag på søknaden din hvis du ikke sender inn
-                          dokumentene vi trenger for å behandle saken din. Ta kontakt med NAV hvis
-                          du ikke får tak i dokumentet
-                        </Alert>
-                      )}
-                  </>
+                  <Spørsmål
+                    key={spørsmål.id}
+                    spørsmål={spørsmål}
+                    formScope={form.scope(spørsmål.id as keyof UtdanningSvar)}
+                  />
                 );
               })}
             </VStack>
+
+            {actionData && (
+              <Alert variant="error" className="mt-4">
+                {actionData.error}
+              </Alert>
+            )}
 
             <HStack gap="4" className="mt-8">
               <Button
