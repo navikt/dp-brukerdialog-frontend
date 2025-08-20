7343990a59c00b750d5d74777859e4f73df24724
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2025-08-20 10:08:54 +0200

    Legge til console.log når sanity data mangler

--- app/hooks/useSanity.tsx
+++ app/hooks/useSanity.tsx
@@ -5,7 +5,10 @@ import { SanityInfoside } from "~/sanity/sanity.types";
 export function useSanity() {
   const data = useRouteLoaderData<typeof loader>("root");
 
-  console.log(data?.sanityData);
+  if (!data?.sanityData) {
+    console.error("useSanity", "Sanity data er ikke tilgjengelig");
+  }
+
   function hentInfosideTekst(textId: string): SanityInfoside | undefined {
     return data?.sanityData?.infosider.find((side) => side.textId === textId);
   }
