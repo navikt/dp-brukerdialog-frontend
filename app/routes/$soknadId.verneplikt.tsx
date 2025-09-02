5e101c11618f111f5793cac657d3a0ba739880a3
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2025-09-02 11:57:14 +0200

    Oppdatert for verneplikt

--- app/routes/$soknadId.verneplikt.tsx
+++ app/routes/$soknadId.verneplikt.tsx
@@ -1,21 +1,9 @@
-import { ArrowLeftIcon, ArrowRightIcon } from "@navikt/aksel-icons";
-import { Alert, Button, HStack, Page, VStack } from "@navikt/ds-react";
-import { useForm } from "@rvf/react-router";
-import {
-  Form,
-  LoaderFunctionArgs,
-  redirect,
-  useActionData,
-  useLoaderData,
-  useNavigate,
-} from "react-router";
+import { LoaderFunctionArgs, redirect } from "react-router";
 import invariant from "tiny-invariant";
-import { Spørsmål } from "~/components/spørsmål/Spørsmål";
-import { useNullstillSkjulteFelter } from "~/hooks/useNullstillSkjulteFelter";
 import { hentSeksjon } from "~/models/hentSeksjon.server";
 import { lagreSeksjon } from "~/models/lagreSeksjon.server";
-import { vernepliktSchema } from "~/seksjon/verneplikt/verneplikt.schema";
-import { vernepliktSpørsmål, VernepliktSvar } from "~/seksjon/verneplikt/verneplikt.spørsmål";
+import { VernepliktSvar } from "~/seksjon/verneplikt/verneplikt.spørsmål";
+import VernepliktView from "~/seksjon/verneplikt/VernepliktView";
 import { parseLoaderData } from "~/utils/loader.utils";
 
 export async function loader({ request, params }: LoaderFunctionArgs) {
@@ -46,73 +34,6 @@ export async function action({ request, params }: LoaderFunctionArgs) {
   return redirect(`/${params.soknadId}/${nesteSeksjonId}`);
 }
 
-export default function Verneplikt() {
-  const actionData = useActionData<typeof action>();
-  const loaderData = useLoaderData<typeof loader>();
-  const navigate = useNavigate();
-
-  const form = useForm({
-    method: "PUT",
-    submitSource: "state",
-    schema: vernepliktSchema,
-    validationBehaviorConfig: {
-      initial: "onBlur",
-      whenTouched: "onBlur",
-      whenSubmitted: "onBlur",
-    },
-    defaultValues: loaderData ?? {},
-  });
-
-  useNullstillSkjulteFelter<VernepliktSvar>(form, vernepliktSpørsmål);
-
-  return (
-    <Page className="brukerdialog">
-      <h2>Verneplikt</h2>
-      <VStack gap="20">
-        <VStack gap="6">
-          <Form {...form.getFormProps()}>
-            <VStack gap="8">
-              {vernepliktSpørsmål.map((spørsmål) => {
-                if (spørsmål.visHvis && !spørsmål.visHvis(form.value())) {
-                  return null;
-                }
-
-                return (
-                  <Spørsmål
-                    key={spørsmål.id}
-                    spørsmål={spørsmål}
-                    formScope={form.scope(spørsmål.id as keyof VernepliktSvar)}
-                  />
-                );
-              })}
-            </VStack>
-
-            {actionData && (
-              <Alert variant="error" className="mt-4">
-                {actionData.error}
-              </Alert>
-            )}
-
-            <HStack gap="4" className="mt-8">
-              <Button
-                variant="secondary"
-                icon={<ArrowLeftIcon title="a11y-title" fontSize="1.5rem" />}
-                onClick={() => navigate(-1)}
-              >
-                Forrige steg
-              </Button>
-              <Button
-                variant="primary"
-                type="submit"
-                iconPosition="right"
-                icon={<ArrowRightIcon />}
-              >
-                Neste steg
-              </Button>
-            </HStack>
-          </Form>
-        </VStack>
-      </VStack>
-    </Page>
-  );
+export default function VernepliktRoute() {
+  return <VernepliktView />;
 }
