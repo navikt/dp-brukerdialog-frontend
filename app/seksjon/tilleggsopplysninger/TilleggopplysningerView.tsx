09a71aea508d9e66db0dd2af009a000d73c9803a
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2025-09-02 12:16:51 +0200

    Fjernet loader og action

--- app/seksjon/tilleggsopplysninger/TilleggopplysningerView.tsx
+++ app/seksjon/tilleggsopplysninger/TilleggopplysningerView.tsx
@@ -1,62 +1,15 @@
 import { ArrowLeftIcon, ArrowRightIcon } from "@navikt/aksel-icons";
 import { Alert, Button, HStack, Page, VStack } from "@navikt/ds-react";
 import { useForm } from "@rvf/react-router";
-import {
-  ActionFunctionArgs,
-  Form,
-  LoaderFunctionArgs,
-  redirect,
-  useActionData,
-  useLoaderData,
-  useNavigate,
-} from "react-router";
+import { Form, useActionData, useLoaderData, useNavigate } from "react-router";
 import { Spørsmål } from "~/components/spørsmål/Spørsmål";
 import { useNullstillSkjulteFelter } from "~/hooks/useNullstillSkjulteFelter";
-import { hentSeksjon } from "~/models/hentSeksjon.server";
-import { lagreSeksjon } from "~/models/lagreSeksjon.server";
+import { action, loader } from "~/routes/$soknadId.tilleggsopplysninger";
 import { tilleggsopplysningerSchema } from "~/seksjon/tilleggsopplysninger/tilleggsopplysninger.schema";
-
-import invariant from "tiny-invariant";
 import {
   tilleggsopplysningerSpørsmål,
   TilleggsopplysningerSvar,
 } from "~/seksjon/tilleggsopplysninger/tilleggsopplysninger.spørsmål";
-import { parseLoaderData } from "~/utils/loader.utils";
-
-export async function loader({ request, params }: LoaderFunctionArgs) {
-  invariant(params.soknadId, "Søknad ID er påkrevd");
-
-  const response = await hentSeksjon(request, params.soknadId, "tilleggsopplysninger");
-
-  if (response.status !== 200) {
-    return undefined;
-  }
-
-  const loaderData: TilleggsopplysningerSvar = await response.json();
-  return parseLoaderData(loaderData);
-}
-
-export async function action({ request, params }: ActionFunctionArgs) {
-  invariant(params.soknadId, "Søknad ID er påkrevd");
-
-  const formData = await request.formData();
-  const seksjonId = "tilleggsopplysninger";
-  const nesteSeksjonId = "oppsummering";
-  const filtrertEntries = Array.from(formData.entries()).filter(
-    ([_, value]) => value !== undefined && value !== "undefined"
-  );
-  const seksjonsData = JSON.stringify(Object.fromEntries(filtrertEntries));
-
-  const response = await lagreSeksjon(request, params.soknadId, seksjonId, seksjonsData);
-
-  if (response.status !== 200) {
-    return {
-      error: "Noe gikk galt ved lagring av din situasjon",
-    };
-  }
-
-  return redirect(`/${params.soknadId}/${nesteSeksjonId}`);
-}
 
 export function TilleggsopplysningerView() {
   const loaderData = useLoaderData<typeof loader>();
