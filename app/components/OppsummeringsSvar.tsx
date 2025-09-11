5f8051a54e74bfa3a17d4497fc9489474e0e054e
Arselan.Sultani <arselan.sultani@nav.no>
2025-09-11 13:25:59 +0200

    Generalisering av felter og lagt til barnetillegg seksjons modul

---
+++ app/components/OppsummeringsSvar.tsx
@@ -0,0 +1,29 @@
+import { KomponentType } from "~/components/spørsmål/spørsmål.types";
+import { FormSummary } from "@navikt/ds-react";
+import { findLandeNavn } from "~/constants";
+import { formaterNorskDato } from "~/utils/formattering.utils";
+
+export default function OppsummeringsSvar({
+  spørsmål,
+  svar,
+}: {
+  spørsmål: KomponentType;
+  svar: string;
+}) {
+  switch (spørsmål.type) {
+    case "kortTekst":
+      return <FormSummary.Value>{svar}</FormSummary.Value>;
+    case "envalg":
+      return (
+        <FormSummary.Value>
+          {spørsmål.options.find((s) => s.value === svar)?.label}
+        </FormSummary.Value>
+      );
+    case "land":
+      return <FormSummary.Value>{findLandeNavn(svar)}</FormSummary.Value>;
+    case "dato":
+      return <FormSummary.Value>{formaterNorskDato(new Date(svar))}</FormSummary.Value>;
+    default:
+      return <></>;
+  }
+}
---
+++ app/seksjon/barnetillegg/oppsummering/BarnetilleggOppsummering.tsx
@@ -0,0 +1,94 @@
+import {
+  barnetilleggSpørsmål,
+  leggTilBarnManueltSpørsmål,
+} from "~/seksjon/barnetillegg/barnetillegg.spørsmål";
+import { BarnetilleggResponse } from "~/routes/$soknadId.barnetillegg";
+import { FormSummary } from "@navikt/ds-react";
+import OppsummeringsSvar from "~/components/OppsummeringsSvar";
+import { formaterNorskDato } from "~/utils/formattering.utils";
+
+export default function BarnetilleggOppsummering({ seksjonsData }: { seksjonsData: string }) {
+  if (seksjonsData === "") {
+    return <></>;
+  }
+
+  const barnetilleggSvar: BarnetilleggResponse = JSON.parse(seksjonsData);
+  const entries = Object.entries(barnetilleggSvar);
+
+  const forsørgerDuBarnSomIkkeVisesHer = barnetilleggSvar.forsørgerDuBarnSomIkkeVisesHer;
+
+  if (forsørgerDuBarnSomIkkeVisesHer === undefined) {
+    return <></>;
+  }
+
+  const forsørgerDuBarnetSpørsmål = barnetilleggSpørsmål.find(
+    (s) => s.id === "forsørgerDuBarnSomIkkeVisesHer"
+  )!;
+
+  const alleBarna = barnetilleggSvar.barnFraPdl?.concat(barnetilleggSvar.barnLagtManuelt ?? []);
+
+  function finnLabelNavn(id: string) {
+    return leggTilBarnManueltSpørsmål.find((spørsmål) => spørsmål.id === id)?.label;
+  }
+
+  return (
+    <FormSummary>
+      <FormSummary.Header>
+        <FormSummary.Heading level="2">Barnetillegg</FormSummary.Heading>
+      </FormSummary.Header>
+      <FormSummary.Answers>
+        {!entries.length && <div>Du har ikke svart på noen spørsmål i denne seksjonen</div>}
+        {
+          <FormSummary.Answer key={"forsørgerDuBarnSomIkkeVisesHer"}>
+            <FormSummary.Label>{forsørgerDuBarnetSpørsmål?.label}</FormSummary.Label>
+            <OppsummeringsSvar
+              spørsmål={forsørgerDuBarnetSpørsmål}
+              svar={forsørgerDuBarnSomIkkeVisesHer}
+            />
+          </FormSummary.Answer>
+        }
+        {alleBarna?.map((barn, index) => {
+          return (
+            <FormSummary.Answer>
+              <FormSummary.Label key={index}>Barn {index + 1}</FormSummary.Label>
+              <FormSummary.Value>
+                <FormSummary.Answers>
+                  <FormSummary.Answer>
+                    <FormSummary.Label>{finnLabelNavn("fornavnOgMellomnavn")}</FormSummary.Label>
+                    <FormSummary.Value>{barn.fornavnOgMellomnavn}</FormSummary.Value>
+                  </FormSummary.Answer>
+                  <FormSummary.Answer>
+                    <FormSummary.Label>{finnLabelNavn("etternavn")}</FormSummary.Label>
+                    <FormSummary.Value>{barn.etternavn}</FormSummary.Value>
+                  </FormSummary.Answer>
+                  <FormSummary.Answer>
+                    <FormSummary.Label>{finnLabelNavn("fødselsdato")}</FormSummary.Label>
+                    <FormSummary.Value>
+                      {formaterNorskDato(new Date(barn.fødselsdato))}
+                    </FormSummary.Value>
+                  </FormSummary.Answer>
+                  <FormSummary.Answer>
+                    <FormSummary.Label>{finnLabelNavn("bostedsland")}</FormSummary.Label>
+                    <FormSummary.Value>{barn.bostedsland}</FormSummary.Value>
+                  </FormSummary.Answer>
+                  <FormSummary.Answer>
+                    <FormSummary.Label>Forsørger du barnet?</FormSummary.Label>
+                    <FormSummary.Value>
+                      {barnetilleggSvar.barnLagtManuelt?.find(
+                        (b) => b.fødselsdato === barn.fødselsdato
+                      )
+                        ? "Ja"
+                        : barn.forsørgerDuBarnet === "ja"
+                          ? "Ja"
+                          : "Nei"}
+                    </FormSummary.Value>
+                  </FormSummary.Answer>
+                </FormSummary.Answers>
+              </FormSummary.Value>
+            </FormSummary.Answer>
+          );
+        })}
+      </FormSummary.Answers>
+    </FormSummary>
+  );
+}
