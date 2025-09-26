113da0446b11b78d47512e2f0862b2596ab215bf
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2025-09-26 13:29:22 +0200

    Fikse bugs for accept fil typer for filopplasting

--- app/utils/dokument.utils.ts
+++ app/utils/dokument.utils.ts
@@ -1,4 +1,4 @@
-export const TILLATTE_FILTYPER = ["pdf", ".jpg", ".jpeg", ".png"];
+export const TILLATTE_FILTYPER = [".pdf", ".jpg", ".jpeg", ".png"];
 export const MAX_ANTALL_FILER = 5;
 export const MAX_FIL_STØRRELSE = 10485760; // 10 MB
 
@@ -8,7 +8,7 @@ export function hentMaksFilStørrelseMB() {
 }
 
 export function hentTillatteFiltyperString() {
-  return TILLATTE_FILTYPER.join(", ");
+  return TILLATTE_FILTYPER.join(",");
 }
 
 export function hentTillatteFiltyperTekst() {
