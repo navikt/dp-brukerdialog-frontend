6990a794f7d544586340be7a3223f4553636ce23
Vegard Skjefstad <vegard.skjefstad@nav.no>
2025-09-30 15:16:53 +0200

    Løsningsforlag på generalisering av superRefine-koden

---
+++ app/utils/validering.util.ts
@@ -0,0 +1,38 @@
+import { KomponentType } from "~/components/spørsmål/spørsmål.types";
+import { $RefinementCtx } from "zod/v4/core";
+
+export function valider(
+  spørsmål: KomponentType,
+  svar: string | string[] | undefined,
+  synlig: boolean,
+  context: $RefinementCtx<any>
+) {
+  const erSpørsmål =
+    spørsmål.type !== "lesMer" &&
+    spørsmål.type !== "varselmelding" &&
+    spørsmål.type !== "dokumentasjonskravindikator";
+
+  if (synlig && !svar && erSpørsmål && !spørsmål.optional) {
+    context.addIssue({
+      path: [spørsmål.id],
+      code: "custom",
+      message: "Du må svare på dette spørsmålet",
+    });
+  }
+  if (svar && spørsmål.type === "tall") {
+    if (isNaN(+(svar as string).replace(",", "."))) {
+      context.addIssue({
+        path: [spørsmål.id],
+        code: "custom",
+        message: "Svaret må være et tall",
+      });
+    }
+    if ((svar as string).startsWith("-")) {
+      context.addIssue({
+        path: [spørsmål.id],
+        code: "custom",
+        message: "Tallet kan ikke være negativt",
+      });
+    }
+  }
+}
