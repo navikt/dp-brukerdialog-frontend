0a74337544fdf379f65a9995248998f328ac1fd7
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2025-11-24 12:33:33 +0100

    Teste verify commit

--- app/seksjon/dokumentasjon/FilOpplasting.tsx
+++ app/seksjon/dokumentasjon/FilOpplasting.tsx
@@ -31,6 +31,8 @@ export enum LastOppFeil {
   UKJENT_FEIL = "UKJENT_FEIL",
 }
 
+// Test verify commit
+
 interface IProps {
   dokumentasjonskrav: Dokumentasjonskrav;
   dokumentkravFiler: DokumentkravFil[];
