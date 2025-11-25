efa82eda86f4633e9bbe4bb063f3561e178a8be6
Nattaphong Klinjan <110396878+nattaphongklinjan@users.noreply.github.com>
2025-11-25 15:26:50 +0100

    Fjernet hardkodet dokumentasjonskrav svar fra kvitteringssiden (#402)
    * Fjernet hardkodet dokumentasjonskrav svar fra kvitteringssiden
    
    * Update app/seksjon/kvittering/DokumentasjonskravSkalSendesAvDeg.tsx
    
    Co-authored-by: ArselanSultani <arselan.sultani@nav.no>
    
    ---------
    
    Co-authored-by: ArselanSultani <arselan.sultani@nav.no>

--- app/routes/$soknadId.kvittering.tsx
+++ app/routes/$soknadId.kvittering.tsx
@@ -1,26 +1,36 @@
+import { LoaderFunctionArgs } from "react-router";
 import invariant from "tiny-invariant";
-import KvitteringView from "~/seksjon/kvittering/KvitteringView";
-import { LoaderFunctionArgs, useLoaderData } from "react-router";
 import { hentAlleSeksjoner } from "~/models/hent-alle-seksjoner.server";
+import { hentDokumentasjonskrav } from "~/models/hent-dokumentasjonskrav.server";
+import { Dokumentasjonskrav } from "~/seksjon/dokumentasjon/DokumentasjonskravKomponent";
+import KvitteringView from "~/seksjon/kvittering/KvitteringView";
+
+export type KvitteringSeksjon = {
+  seksjoner: [] | null;
+  dokumentasjonskrav: Dokumentasjonskrav[] | null;
+};
 
 export async function loader({ request, params }: LoaderFunctionArgs) {
   invariant(params.soknadId, "Søknad ID er påkrevd");
 
-  const response = await hentAlleSeksjoner(request, params.soknadId);
+  const seksjonerResponse = await hentAlleSeksjoner(request, params.soknadId);
+  const dokumentasjonskravResponse = await hentDokumentasjonskrav(request, params.soknadId);
 
-  if (response.ok) {
-    return await response.json();
+  if (seksjonerResponse.ok && dokumentasjonskravResponse.ok) {
+    const dokumentasjonskravData = await dokumentasjonskravResponse.json();
+
+    return {
+      seksjoner: await seksjonerResponse.json(),
+      dokumentasjonskrav: dokumentasjonskravData.flatMap((krav: string) => JSON.parse(krav)),
+    };
   }
 
-  return null;
+  return {
+    seksjoner: null,
+    dokumentasjonskrav: null,
+  };
 }
 
 export default function Kvittering() {
-  const loaderData = useLoaderData<typeof loader>();
-
-  if (!loaderData) {
-    return null;
-  }
-
   return <KvitteringView />;
 }
---
+++ app/seksjon/kvittering/DokumentasjonskravIkkeSkalSendes.tsx
@@ -0,0 +1,30 @@
+import { ComponentIcon } from "@navikt/aksel-icons";
+import { BodyShort, Box, Heading, HStack, ReadMore, Tag, VStack } from "@navikt/ds-react";
+import { DokumentasjonskravInnhold } from "../dokumentasjon/DokumentasjonskravInnhold";
+import { Dokumentasjonskrav } from "../dokumentasjon/DokumentasjonskravKomponent";
+
+interface IProps {
+  dokummentasjonskrav: Dokumentasjonskrav;
+}
+
+export default function DokumentasjonskravIkkeSkalSendes({ dokummentasjonskrav }: IProps) {
+  return (
+    <Box padding="space-16" background="surface-action-subtle" borderRadius="large">
+      <VStack gap="2">
+        <VStack gap="2">
+          <HStack gap="2" justify="space-between">
+            <Heading size="xsmall" level="4">
+              {dokummentasjonskrav.tittel}
+            </Heading>
+          </HStack>
+
+          <BodyShort>Du har sagt at du ikke sender dette</BodyShort>
+        </VStack>
+
+        <ReadMore header="Dette må dokumentasjonen inneholde">
+          <DokumentasjonskravInnhold type={dokummentasjonskrav.type} />
+        </ReadMore>
+      </VStack>
+    </Box>
+  );
+}
