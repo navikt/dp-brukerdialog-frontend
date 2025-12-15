aa68e9e99e30792b6db171077e6b127af2162f01
Vegard Skjefstad <vegard.skjefstad@nav.no>
2025-12-15 13:48:47 +0100

    Oppdater seksjon så den blir lik skissen

--- app/seksjon/din-situasjon/v1/din-situasjon.komponenter.ts
+++ app/seksjon/din-situasjon/v1/din-situasjon.komponenter.ts
@@ -19,7 +19,7 @@ export const dinSituasjonKomponenter: KomponentType[] = [
   {
     id: harDuMottattDagpengerFraNavILøpetAvDeSiste52Ukene,
     type: "envalg",
-    label: "Har du mottatt dagpenger fra NAV i løpet av de siste 52 ukene?",
+    label: "Har du mottatt dagpenger fra Nav i løpet av de siste 52 ukene?",
     options: [
       { value: "ja", label: "Ja" },
       { value: "nei", label: "Nei" },
@@ -29,8 +29,30 @@ export const dinSituasjonKomponenter: KomponentType[] = [
   {
     id: årsakTilAtDagpengeneBleStanset,
     type: "langTekst",
-    label: "Skriv årsaken til at dagpengene ble stanset (Maks 500 tegn)",
+    label: "Skriv om årsaken til at dagpengene ble stanset",
+    description:
+      "For eksempel om du har hatt arbeid, vært syk, på ferie, glemt å sende meldekort, vært i utdanning eller hatt foreldrepermisjon.",
     maxLength: 500,
+    visHvis: (svar: DinSituasjonSvar) =>
+      svar[harDuMottattDagpengerFraNavILøpetAvDeSiste52Ukene] === "ja",
+  },
+  {
+    id: hvilkenDatoSøkerDuGjenopptakFra,
+    type: "dato",
+    label: "Hvilken dato søker du gjenopptak av dagpenger fra?",
+    description:
+      "Hvis du har jobbet siden sist skal du oppgi den første dagen du ble helt eller delvis arbeidsledig.",
+    fraOgMed: startOfDay(subMonths(new Date(), 6)),
+    tilOgMed: endOfDay(addMonths(new Date(), 3)),
+    visHvis: (svar: DinSituasjonSvar) =>
+      svar[harDuMottattDagpengerFraNavILøpetAvDeSiste52Ukene] === "ja",
+  },
+  {
+    id: "hvilkenDatoSøkerDuGjenopptakFraLesMer",
+    type: "lesMer",
+    label: "Les om når du tidligst kan få dagpenger fra",
+    description:
+      "Du kan tidligst få dagpenger fra den dagen du sender inn søknaden om gjenopptak om dagpenger.",
     visHvis: (svar: DinSituasjonSvar) =>
       svar[harDuMottattDagpengerFraNavILøpetAvDeSiste52Ukene] === "ja",
   },
@@ -40,43 +62,22 @@ export const dinSituasjonKomponenter: KomponentType[] = [
     label: "Hvilken dato søker du dagpenger fra?",
     description:
       "Du kan få dagpenger fra første dag du er helt eller delvis arbeidsledig eller permittert og tidligst fra den dagen du sender inn søknaden. Datoen du søker om dagpenger fra har betydning for beregning av dagpengene dine.",
+    fraOgMed: startOfDay(subMonths(new Date(), 6)),
+    tilOgMed: endOfDay(addMonths(new Date(), 3)),
     visHvis: (svar: DinSituasjonSvar) =>
       svar[harDuMottattDagpengerFraNavILøpetAvDeSiste52Ukene] === "nei" ||
       svar[harDuMottattDagpengerFraNavILøpetAvDeSiste52Ukene] === "vetikke",
-    fraOgMed: startOfDay(subMonths(new Date(), 6)),
-    tilOgMed: endOfDay(addMonths(new Date(), 3)),
   },
   {
     id: "hvilkenDatoSøkerDuDagpengerFraLesMer",
     type: "lesMer",
     label: "Dato, inntekt og beregning av dagpenger",
     description:
-      "Vi beregner hvor mye du kan få i dagpenger basert på hva du har hatt i inntekt de siste 12 eller 36 månedene. Ønsker du å få med siste måneds inntekt, må du søke dagpenger tidligst fra den 6. i måneden etter. Årsaken er at vi får melding om inntekten din fra Skatteetaten den 5. i måneden. Hvis den 5. er en helg eller helligdag, får vi melding om inntekten din neste virkedag.<br/><br/>" +
-      "<strong>Eksempel:</strong><br/><br/>" +
-      "Hvis du søker om dagpenger fra 1. til 5. mars, vil januar bli siste måned med inntekt som tas med i beregningen av dagpengene dine. Søker du om dagpenger fra 6. mars, vil februar bli siste måned med inntekt som tas med i beregningen.<br/><br/>" +
-      "Hvis 5. mars er en lørdag, får vi inn inntekten din mandag 7. mars. Da må du søke dagpenger fra tirsdag 8. mars hvis du vil ha med inntekten din fra februar i beregningen.",
+      "<p>Vi beregner hvor mye du kan få i dagpenger basert på hva du har hatt i inntekt de siste 12 eller 36 månedene. Ønsker du å få med siste måneds inntekt, må du søke dagpenger tidligst fra den 6. i måneden etter. Årsaken er at vi får melding om inntekten din fra Skatteetaten den 5. i måneden. Hvis den 5. er en helg eller helligdag, får vi melding om inntekten din neste virkedag.</p>" +
+      "<p><strong>Eksempel:</strong><br/>Hvis du søker om dagpenger fra 1. til 5. mars, vil januar bli siste måned med inntekt som tas med i beregningen av dagpengene dine. Søker du om dagpenger fra 6. mars, vil februar bli siste måned med inntekt som tas med i beregningen.</p>" +
+      "<p>Hvis 5. mars er en lørdag, får vi inn inntekten din mandag 7. mars. Da må du søke dagpenger fra tirsdag 8. mars hvis du vil ha med inntekten din fra februar i beregningen.</p>",
     visHvis: (svar: DinSituasjonSvar) =>
       svar[harDuMottattDagpengerFraNavILøpetAvDeSiste52Ukene] === "nei" ||
       svar[harDuMottattDagpengerFraNavILøpetAvDeSiste52Ukene] === "vetikke",
-  },
-  {
-    id: hvilkenDatoSøkerDuGjenopptakFra,
-    type: "dato",
-    label: "Hvilken dato søker du gjenopptak av dagpenger fra?",
-    description:
-      "Hvis du har jobbet siden sist skal du oppgi den første dagen du ble helt eller delvis arbeidsledig.",
-    visHvis: (svar: DinSituasjonSvar) =>
-      svar[harDuMottattDagpengerFraNavILøpetAvDeSiste52Ukene] === "ja",
-    fraOgMed: startOfDay(subMonths(new Date(), 6)),
-    tilOgMed: endOfDay(addMonths(new Date(), 3)),
-  },
-  {
-    id: "hvilkenDatoSøkerDuGjenopptakFraLesMer",
-    type: "lesMer",
-    label: "Les om når du tidligst kan få dagpenger fra ",
-    description:
-      "Du kan tidligst få dagpenger fra den dagen du sender inn søknaden om gjenopptak om dagpenger.",
-    visHvis: (svar: DinSituasjonSvar) =>
-      svar[harDuMottattDagpengerFraNavILøpetAvDeSiste52Ukene] === "ja",
   },
 ];
