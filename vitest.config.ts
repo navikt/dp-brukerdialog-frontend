6c54b2e8f02e0532269ffb2ec0e9a8b7224d0e78
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2025-12-16 12:27:01 +0100

    Konfigurer test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,20 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["**/*.test.ts"], // Kun filer som slutter på .test.ts
+    exclude: ["**/node_modules/**", "**/.react-router/**"],
+    coverage: {
+      include: ["**/*.{ts,tsx}"],
+      exclude: [
+        "**/.react-router/**",
+        "**/routes.ts",
+        "**/*.config.ts",
+        "**/*.types.ts",
+        "**/mocks/**",
+        "**/sanity/**",
+        "app/entry.client.tsx",
+        "app/entry.server.tsx",
+      ],
+      provider: 'v8', // or 'istanbul'
+    },
   },
 });
