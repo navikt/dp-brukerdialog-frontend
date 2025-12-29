62a6b097ce2abd1ed8778a1cc2d9922f57ad4b44
Vegard Skjefstad <vegard.skjefstad@nav.no>
2025-12-29 10:56:54 +0100

    Bruk aria-label på "Endre svar"-linkene
    Forteller skjermleser hvilke seksjon linken tar bruker til

--- app/seksjon/oppsummering/FormSummaryFooter.tsx
+++ app/seksjon/oppsummering/FormSummaryFooter.tsx
@@ -1,16 +1,18 @@
 import { FormSummary } from "@navikt/ds-react";
 
 export default function FormSummaryFooter({
+  seksjonnavn,
   seksjonsUrl,
   redigerbar,
 }: {
+  seksjonnavn: string;
   seksjonsUrl: string;
   redigerbar: boolean;
 }) {
   if (!redigerbar) return null;
   return (
     <FormSummary.Footer>
-      <FormSummary.EditLink href={seksjonsUrl} />
+      <FormSummary.EditLink href={seksjonsUrl} aria-label={`Endre dine svar om ${seksjonnavn}`} />
     </FormSummary.Footer>
   );
 }
