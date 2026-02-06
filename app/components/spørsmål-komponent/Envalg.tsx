75259d4f1aed81c14dcbe36ff68a23956c850fd4
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2026-02-06 14:06:23 +0100

    Legge til eksempel på feilmelding fokus

--- app/components/spørsmål-komponent/Envalg.tsx
+++ app/components/spørsmål-komponent/Envalg.tsx
@@ -2,6 +2,7 @@ import { HStack, Radio, RadioGroup } from "@navikt/ds-react";
 import { FormScope, useField } from "@rvf/react-router";
 import parse from "html-react-parser";
 import { EnvalgSpørsmål } from "../Komponent.types";
+import { useEffect, useRef } from "react";
 
 interface IProps {
   props: EnvalgSpørsmål;
@@ -11,11 +12,33 @@ interface IProps {
 
 export function Envalg({ props, formScope, horisontal }: IProps) {
   const field = useField(formScope);
+  const ref = useRef<HTMLFieldSetElement>(null);
+
+  useEffect(() => {
+    if (field.error()) {
+      console.log("🎯 Setting focus on RadioGroup", field.error());
+      ref.current?.focus();
+      ref.current?.scrollIntoView({ behavior: "smooth", block: "center" });
+
+      // Debug styling
+      if (ref.current) {
+        ref.current.style.backgroundColor = "yellow";
+        ref.current.style.outline = "3px solid red";
+      }
+    } else {
+      // Remove debug styling
+      if (ref.current) {
+        ref.current.style.backgroundColor = "";
+        ref.current.style.outline = "";
+      }
+    }
+  }, [field.error()]);
 
   return (
     <RadioGroup
       {...field.getInputProps()}
       legend={props.label}
+      ref={ref}
       key={props.id}
       defaultValue={(field.value() as string) ?? undefined}
       description={parse(props?.description ?? "", { trim: true })} // TODO: Få denne til å parse react-komponenter?
