eb09419fa628fda45c9c14711be43a8c2babcba7
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2026-04-09 09:53:45 +0200

    Fjerned unsued import

--- app/components/Komponent.tsx
+++ app/components/Komponent.tsx
@@ -1,4 +1,3 @@
-import { Heading } from "@navikt/ds-react";
 import { FormScope } from "@rvf/react-router";
 import { useEffect, useRef } from "react";
 import { Dokumentasjonskravindikator } from "~/components/informasjon-komponent/Dokumentasjonskravindikator";
@@ -7,6 +6,7 @@ import { Nedtrekksliste } from "~/components/spørsmål-komponent/Nedtrekksliste
 import { Tall } from "~/components/spørsmål-komponent/Tall";
 import { useFokus } from "~/hooks/useFokus";
 import { useSoknad } from "~/seksjon/soknad.context";
+import { HeadingTekst } from "./informasjon-komponent/HeadingTekst";
 import { Informasjonskort } from "./informasjon-komponent/Informasjonskort";
 import { LesMer } from "./informasjon-komponent/LesMer";
 import { KomponentType } from "./Komponent.types";
@@ -17,7 +17,6 @@ import { KortTekst } from "./spørsmål-komponent/KortTekst";
 import { Land } from "./spørsmål-komponent/Land";
 import { LangTekst } from "./spørsmål-komponent/LangTekst";
 import { Periode } from "./spørsmål-komponent/Periode";
-import { HeadingTekst } from "./informasjon-komponent/HeadingTekst";
 
 interface IProps {
   props: KomponentType;
