ffe7902e8c4ca92a0ea753b96d27fb2e5b3a0a8f
Nattaphong Klinjan <110396878+nattaphongklinjan@users.noreply.github.com>
2026-04-13 14:35:30 +0200

    Gjenbruke lagreSeksjon (#645)

--- app/routes/api.$soknadId.$seksjonId.$versjon.tsx
+++ app/routes/api.$soknadId.$seksjonId.$versjon.tsx
@@ -1,16 +1,13 @@
 import { parseFormData } from "@remix-run/form-data-parser";
 import { ActionFunctionArgs } from "react-router";
 import invariant from "tiny-invariant";
-import { hentSoknadOrkestratorOboToken } from "~/utils/auth.utils.server";
-import { getEnv } from "~/utils/env.utils";
+import { lagreSeksjon } from "~/models/lagre-seksjon.server";
 
 export async function action({ request, params }: ActionFunctionArgs) {
   invariant(params.soknadId, "Søknad ID er påkrevd");
   invariant(params.seksjonId, "Seksjon ID er påkrevd");
   invariant(params.versjon, "Versjon er påkrevd");
 
-  const url = `${getEnv("DP_SOKNAD_ORKESTRATOR_URL")}/seksjon/${params.soknadId}/${params.seksjonId}`;
-  const onBehalfOfToken = await hentSoknadOrkestratorOboToken(request);
   const formData = await parseFormData(request);
   const pdfGrunnlag = formData.get("pdfGrunnlag") as string;
 
@@ -22,14 +19,5 @@ export async function action({ request, params }: ActionFunctionArgs) {
     pdfGrunnlag: pdfGrunnlag,
   };
 
-  return await fetch(url, {
-    method: "PUT",
-    headers: {
-      Accept: "application/json",
-      Authorization: `Bearer ${onBehalfOfToken}`,
-      connection: "keep-alive",
-      "Content-Type": "application/json; charset=UTF-8",
-    },
-    body: JSON.stringify(putSeksjonRequestBody),
-  });
+  return await lagreSeksjon(request, params.soknadId, params.seksjonId, putSeksjonRequestBody);
 }
