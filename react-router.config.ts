d7cf266dec06ef525a2c3e606a6caff26b834420
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2026-04-14 10:39:16 +0200

    Trailing slash på base, uten på basePath

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,5 +2,5 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   ssr: true,
-  basename: "/ny-dagpenger/dialog",
+  basename: "/ny-dagpenger/dialog/",
 } satisfies Config;
