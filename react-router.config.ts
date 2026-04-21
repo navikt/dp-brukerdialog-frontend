14758798a9deb9c2697c8f428ca664f5ea0916d6
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2026-04-21 12:35:19 +0200

    Oppdatere ingress, gjør appen klar til å ta over ingressen over dp-soknadsdialog

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,5 +2,5 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   ssr: true,
-  basename: "/ny-dagpenger/dialog/soknad",
+  basename: "/dagpenger/dialog/soknad",
 } satisfies Config;
