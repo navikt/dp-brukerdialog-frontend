2541b245edd25e7be16282884fc9347b1a43744e
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2026-06-10 17:14:26 +0200

    Håndtere /gammel-dagpenger url redirect

--- app/utils/redirect.utils.ts
+++ app/utils/redirect.utils.ts
@@ -1,18 +1,28 @@
-const GAMMEL_INGRESS_PREFIX = "/ny-dagpenger/dialog/soknad";
-const URL_PREFIX = "/dagpenger/dialog/soknad";
+const UTDAGERT_INGRESS_PREFIX = "ny-dagpenger";
+const GAMMEL_SØKNAD_INGRESS_PREFIX = "gammel-dagpenger";
+export const GAMMEL_SØKNAD_REDIRECT_URL = "/dagpenger/dialog/soknad/info-side";
 
-export function isOutdatedBasePath(request: Request): boolean {
+const NY_DAGPENGER_REGEX = new RegExp(`^/${UTDAGERT_INGRESS_PREFIX}(?:/|$)`);
+const GAMMEL_DAGPENGER_REGEX = new RegExp(`^/${GAMMEL_SØKNAD_INGRESS_PREFIX}(?:/|$)`);
+
+export function erUtdatertBasePath(request: Request): boolean {
   const { pathname } = new URL(request.url);
-  return pathname === GAMMEL_INGRESS_PREFIX || pathname.startsWith(`${GAMMEL_INGRESS_PREFIX}/`);
+  return NY_DAGPENGER_REGEX.test(pathname);
 }
 
-export function createUpdatedBasePath(pathname: string): string {
-  return URL_PREFIX + pathname.slice(GAMMEL_INGRESS_PREFIX.length);
+export function erGammelSøknadBasePath(request: Request): boolean {
+  const { pathname } = new URL(request.url);
+  return GAMMEL_DAGPENGER_REGEX.test(pathname);
 }
 
-export function RedirectToUpdatedBasePath(url: URL): Response {
-  return Response.redirect(
-    new URL(createUpdatedBasePath(url.pathname) + url.search, url.origin).toString(),
-    301
-  );
+export function redirectTilOppdatertIngress(url: URL): Response {
+  const erRotPath = /^\/ny-dagpenger\/?$/.test(url.pathname);
+  const oppdatertBasePath = erRotPath
+    ? "/dagpenger/dialog/soknad/"
+    : url.pathname.replace(NY_DAGPENGER_REGEX, "/dagpenger/");
+  return Response.redirect(new URL(oppdatertBasePath + url.search, url.origin).toString(), 301);
+}
+
+export function redirectTilInfoside(url: URL): Response {
+  return Response.redirect(new URL(GAMMEL_SØKNAD_REDIRECT_URL, url.origin).toString(), 302);
 }
