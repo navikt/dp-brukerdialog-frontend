158ecbd68933bdc3b087016e89b0d10a754c5f5c
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2026-06-11 15:40:16 +0200

    Oppdatere tekst

--- app/routes/info-side.tsx
+++ app/routes/info-side.tsx
@@ -1,12 +1,24 @@
-import { BodyLong } from "@navikt/ds-react";
+import { BodyLong, Heading, VStack } from "@navikt/ds-react";
+import { EksterneLenke } from "~/components/EksterneLenke";
+import { getEnv } from "~/utils/env.utils";
 
 export default function Infoside() {
   return (
     <main id="maincontent" tabIndex={-1}>
       <title>Infoside</title>
-      <BodyLong>
-        Din søknad om dagpenger har blitt mottatt og arkivert. Sjekk Mine dagpenger
-      </BodyLong>
+      <Heading size="large" level="1" spacing>
+        Din søknad om dagpenger har blitt mottatt og arkivert
+      </Heading>
+
+      <VStack gap="space-32">
+        <BodyLong>
+          Nav har mottatt søknaden din om dagpenger, og den er nå arkivert. Søknaden blir behandlet
+          så snart som mulig. Du vil få en bekreftelse på e-post når behandlingen er ferdig.
+        </BodyLong>
+        <BodyLong>Den arkiverte søknaden finner du i Dokumentoversikt på Mine dagpenger.</BodyLong>
+
+        <EksterneLenke asButton href={getEnv("DP_MINE_DAGPENGER_URL")} tekst="Mine dagpenger" />
+      </VStack>
     </main>
   );
 }
