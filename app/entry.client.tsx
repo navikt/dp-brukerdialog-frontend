bb7143213c425b0648b38ec432d09d5ac903f5b6
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2026-08-11 15:25:30 +0200

    initialize i18n før den brukes

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -4,9 +4,10 @@
  * For more information, see https://remix.run/file-conventions/entry.client
  */
 
-import { HydratedRouter } from "react-router/dom";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
+import { HydratedRouter } from "react-router/dom";
+import "./i18n";
 import { initFaro } from "./utils/faro.utils";
 
 try {
