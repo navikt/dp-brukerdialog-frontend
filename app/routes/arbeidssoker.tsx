e1e0072b901ebe1183603936457680f9904f9a29
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2026-08-27 14:27:41 +0200

    Clientside redirect dersom bruker prøver å komme inn på siden mens bruker er allere registrert som arbeidssøker

--- app/routes/arbeidssoker.tsx
+++ app/routes/arbeidssoker.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { useTypedRouteLoaderData } from "~/hooks/useTypedRouteLoaderData";
 import { ArbeidssøkerView } from "~/seksjon/arbeidssøker/ArbeidssøkerView";
 
 export default function ArbeidssokerSide() {
+  const navigate = useNavigate();
+  const { arbeidssøkerStatus } = useTypedRouteLoaderData("root");
+
+  useEffect(() => {
+    if (arbeidssøkerStatus === "REGISTRERT") {
+      navigate("/opprett-soknad", { replace: true });
+    }
+  }, [arbeidssøkerStatus, navigate]);
+
   return (
     <main id="maincontent" tabIndex={-1}>
       <ArbeidssøkerView />
