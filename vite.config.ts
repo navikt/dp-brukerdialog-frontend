b504f944bf38f5f4a0e2d7cb10a7e8fa40ce70a4
Elisabeth Kongshavn <elisabeth_kongshavn@hotmail.com>
2026-09-01 11:56:48 +0200

    bumpe pakker

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,19 @@
 import { reactRouter } from "@react-router/dev/vite";
-import path from "path";
 import { defineConfig } from "vite";
 import devtoolsJson from "vite-plugin-devtools-json";
-import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   base:
-    process.env.NODE_ENV === "production"
-      ? "https://cdn.nav.no/teamdagpenger/dp-brukerdialog-frontend/client/"
-      : "/dagpenger/dialog/soknad",
-  plugins: [reactRouter(), tsconfigPaths(), devtoolsJson()],
+    command === "build" ? "https://cdn.nav.no/teamdagpenger/dp-brukerdialog-frontend/client/" : "/",
+
+  plugins: [reactRouter(), devtoolsJson()],
+
   build: {
     manifest: true,
     sourcemap: process.env.NODE_ENV !== "production",
   },
+
   resolve: {
-    alias: {
-      "~": path.resolve(__dirname, "./app"),
-    },
+    tsconfigPaths: true,
   },
-});
+}));
